372345a00358c629d0a4ee96e4bcc9e40f8027c2
Matteo <im4thewofficial@libero.it>
2022-08-13 22:44:37 +0200

    ciao negro

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ package dev.maath.mathtools;
 
 public class Main {
     public static void main (String[] args) {
-        System.out.println("porcodio");
+        System.out.println("porcodio godo");
     }
 }
